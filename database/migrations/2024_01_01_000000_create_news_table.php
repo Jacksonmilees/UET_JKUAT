8beae52cee26a2462531662fa054a6dd88197934
Jackson <jacksonmilees@gmail.com>
2025-12-14 01:46:42 +0300

    deploy

--- database/migrations/2024_01_01_000000_create_news_table.php
+++ database/migrations/2024_01_01_000000_create_news_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('image_url')->nullable();
             $table->string('author')->nullable();
             $table->boolean('published')->default(true);
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
